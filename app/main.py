00189b9ff6cbe7296b139ddb418659aad66f3eb9
Delzarkafashi <145854766+Delzarkafashi@users.noreply.github.com>
2025-10-24 18:39:15 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,12 +2,15 @@
 from flask import Flask, render_template, request
 import requests
 
+
 app = Flask(__name__)  # funkar direkt om mapparna ligger i app/
+
 
 def get_random_image() -> str:
     r = requests.get("https://dog.ceo/api/breeds/image/random", timeout=10)
     r.raise_for_status()
     return r.json()["message"]
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -19,6 +22,7 @@ def index():
         except Exception as e:
             error = str(e)
     return render_template("index.html", image_url=img_url, error=error)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
