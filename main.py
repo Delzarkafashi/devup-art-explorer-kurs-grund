d6c8296aa7e959b26cb5907e258195e425e0edfb
Delzarkafashi <145854766+Delzarkafashi@users.noreply.github.com>
2025-10-24 17:27:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template, request
 import requests
 
+
 app = Flask(__name__)
+
 
 def get_random_image() -> str:
     r = requests.get("https://dog.ceo/api/breeds/image/random", timeout=10)
     r.raise_for_status()
     return r.json()["message"]
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -18,6 +21,7 @@ def index():
         except Exception as e:
             error = str(e)
     return render_template("index.html", image_url=img_url, error=error)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
